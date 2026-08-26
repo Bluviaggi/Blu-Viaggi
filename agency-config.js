7b7eaedf34a4fd16db520fa1602fc2d81605f844
Bluviaggi <infobluviaggi@gmail.com>
2026-08-26 18:57:57 +0200

    agency-config.js

---
+++ agency-config.js
@@ -0,0 +1,26 @@
+const AGENCY_CONFIG = {
+
+  name: "Blu Viaggi",
+
+  phone: "393762884036",
+
+  displayPhone: "+39 376 288 4036",
+
+  whatsappMessage:
+    "Ciao Blu Viaggi, vorrei informazioni su un viaggio.",
+
+  transport:
+    "Autobus Gran Turismo e traghetto dove previsto",
+
+  meals:
+    "Pensione completa",
+
+  notes:
+    "Prezzi indicativi per persona. Verificare disponibilità e condizioni prima della conferma.",
+
+  months: [
+    "Settembre 2026",
+    "Ottobre 2026"
+  ]
+
+};
